07635c7a0770520a7f82d65cc035ef430df7358c
yussyamelia <yussyamelia14@gmail.com>
2025-11-13 11:21:51 +0700

    Update luaslingkaran.php

--- luaslingkaran.php
+++ luaslingkaran.php
@@ -1,13 +1,34 @@
 <?php
 
+namespace App\Math;
+
 class LuasLingkaran {
 
     public const phi = 3.14;
-    public int $jari;
+    private int $jari;
+    
+    public function __construct($isiJari = 1) {
+        $this->jari = $isiJari;
+    }
+
+    public function tampil($nama = 'ban') {
+        $rumus = LuasLingkaran::phi * ($this->jari * $this->jari);
+        echo "Lingkaran {$nama} hasilnya adalah: {$rumus}";
+    }
+
+    public static function testing() {
+        echo "<br/>";
+        echo "ini dari static";
+    }
+
+    public function __destruct() {
+        echo "<br/>";
+        echo "udah ah cape";
+    }
 }
 
-$lingkaran = new LuasLingkaran();
-$lingkaran->jari = 12;
+$lingkaran = new LuasLingkaran(10);
+//$lingkaran->jari = 10;
+$lingkaran->tampil('roda'); //panggil method tampil
 
-$rumus = LuasLingkaran::phi * ($lingkaran->jari * $lingkaran->jari);
-echo "Hasilnya adalah: ".$rumus;
+//LuasLingkaran::testing(); //panggil static method
