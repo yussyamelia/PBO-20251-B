c9440e6b5da5153c081faa1d327474d636a339fc
yussyamelia <yussyamelia14@gmail.com>
2025-11-13 11:17:25 +0700

    Add files via upload

---
+++ luaslingkaran.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Math;
+
+class LuasLingkaran {
+
+    public const phi = 3.14;
+    private int $jari;
+    
+    public function __construct($isiJari = 1) {
+        $this->jari = $isiJari;
+    }
+
+    public function tampil($nama = 'ban') {
+        $rumus = LuasLingkaran::phi * ($this->jari * $this->jari);
+        echo "Lingkaran {$nama} hasilnya adalah: {$rumus}";
+    }
+
+    public static function testing() {
+        echo "<br/>";
+        echo "ini dari static";
+    }
+
+    public function __destruct() {
+        echo "<br/>";
+        echo "udah ah cape";
+    }
+}
+
+$lingkaran = new LuasLingkaran(10);
+//$lingkaran->jari = 10;
+$lingkaran->tampil('roda'); //panggil method tampil
+
+//LuasLingkaran::testing(); //panggil static method
